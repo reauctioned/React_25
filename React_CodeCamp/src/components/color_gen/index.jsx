ca29f654160d59ff2f220a11ad0e57c616d41052
Saurav <sauravgamer441@gmail.com>
2024-07-23 08:42:18 +0545

    Random color functional

--- React_CodeCamp/src/components/color_gen/index.jsx
+++ React_CodeCamp/src/components/color_gen/index.jsx
@@ -1,24 +1,24 @@
-import React,{useState} from "react";
+import React,{useState, useEffect} from "react";
 import "./styles.css"
 
 export default function RandomColor(){
-    const [typeOfColor,setTypeOfColor] = useState('hex')
-    const [color,setColor] = useState('#000000')
+    const [typeOfColor,setTypeOfColor] = useState("hex")
+    const [color,setColor] = useState("#000000")
     
-console.log(handleRandomColor)
+
    
-   function randomColorUtility(){
+   function randomColorUtility(length){
     return Math.floor(Math.random()*length)
    } 
 
   function handleRandomHexColor(){
-    const hex = [1,2,3,4,5,6,7,8,9,0,'A','B','C','D','E','F']
-    let hash ='#'
+    let hex = [1,2,3,4,5,6,7,8,9,0,'A','B','C','D','E','F']
+    let hexColor ='#'
 
-    for(i = 0 ; i < 6 ; i ++){
-         hex += hex[randomColorUtility(hex.length)]
+    for( var i = 0 ; i < 6 ; i ++){
+         hexColor += hex[randomColorUtility(hex.length)]
        }
-       setColor()
+       setColor(hexColor)
   }
 
 
@@ -27,21 +27,28 @@ console.log(handleRandomColor)
    const r = randomColorUtility(256)
    const g = randomColorUtility(256)
    const b = randomColorUtility(256)
-   setColor(`rgb${r},${g},${b}`)
+   setColor(`rgb(${r},${g},${b})`)
   }
 
-  function handleRandomColor(){}
+
+
+  useEffect(() => {
+    if (typeOfColor === "rgb") handleRandomRbgColor();
+    else handleRandomHexColor();
+  }, [typeOfColor]);
 
 
    return ( 
    
-   <div className="wrapper" style={{background: color, height: "100%" , width: "100%"}}>
-        <button onClick={() => setTypeOfColor('hex')}>Hex Button</button>
-        <button onClick={() => setTypeOfColor('rgb')}>Rgb Button</button>
-        <button onClick={handleRandomColor()}>Random Button</button>
-           <div style={{display:"flex", justifyContent: "center", color: "white"
+   <div className="wrapper" style={{background: color, height: "100vh" , width: "90vw" , borderRadius:"2%"}}>
+        <button onClick={() => setTypeOfColor("hex")}>Hex Button</button>
+        <button onClick={() => setTypeOfColor("rgb")}>Rgb Button</button>
+        <button onClick={
+          typeOfColor === 'hex' ? handleRandomHexColor : handleRandomRbgColor
+        }>Random Button</button>
+           <div style={{display:"flex", justifyContent: "center", color: "white" ,marginLeft: '10%',fontSize:'30px',marginBottom:"50px"
            }}>
-            <h3>{typeOfColor === 'rbg'? "RGB" : "HEX"}</h3>
+            <h3>{typeOfColor === "rgb"? "RGB" : "HEX"}</h3>
             <h3>{color}</h3>
            </div>
 
