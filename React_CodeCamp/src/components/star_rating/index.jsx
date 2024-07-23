7d75c2c5c8f3d1bc8aa5341bc11e7e122969dc36
Saurav <sauravgamer441@gmail.com>
2024-07-23 19:37:45 +0545

    added additional functionality to deselect 1 star selection

--- React_CodeCamp/src/components/star_rating/index.jsx
+++ React_CodeCamp/src/components/star_rating/index.jsx
@@ -1,26 +1,33 @@
 import {FaStar } from 'react-icons/fa'
 import { useState } from 'react'
+import './styles.css'
 
-export default function StarRating(noOfStars){
+export default function StarRating({noOfStars = 5}){
 
     const [rating,setRating] = useState(0)
     const [hover,setHover] = useState(0)
 
 
-    function handleMouseClick(currentIndex){}
+    function handleMouseClick(currentIndex){ if (currentIndex === rating) {
+        setRating(0);
+      } else {
+        setRating(currentIndex);
+      }
+    }
 
-    function handleMouseEnter(currentIndex){}
+    function handleMouseEnter(currentIndex){setHover(currentIndex);}
 
-    function handleMouseExit(currentIndex){}
+    function handleMouseExit(currentIndex){setHover(rating);}
 
 return <div className="star-rating">
     {
         [...Array(noOfStars)].map((_,index)=> {
 
-         index =+ 1
+         index += 1
 
         return <FaStar
        key={index}
+       className = {index <= (hover || rating )? 'active': ' inactive'}
        onClick={()=> handleMouseClick(index)}
        onMouseMove={()=> handleMouseEnter(index)}
        onMouseLeave = {()=> handleMouseExit(index)}
