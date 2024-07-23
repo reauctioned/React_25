96ce92bfa6c2e730a8c001f837d21542e40d1e29
Saurav <sauravgamer441@gmail.com>
2024-07-23 08:46:07 +0545

    Squashed non functional rgb button bug'

--- React_CodeCamp/src/components/color_gen/index.jsx
+++ React_CodeCamp/src/components/color_gen/index.jsx
@@ -27,6 +27,8 @@ export default function RandomColor(){
    const r = randomColorUtility(256)
    const g = randomColorUtility(256)
    const b = randomColorUtility(256)
+   
+
    setColor(`rgb(${r},${g},${b})`)
   }
 
@@ -44,11 +46,11 @@ export default function RandomColor(){
         <button onClick={() => setTypeOfColor("hex")}>Hex Button</button>
         <button onClick={() => setTypeOfColor("rgb")}>Rgb Button</button>
         <button onClick={
-          typeOfColor === 'hex' ? handleRandomHexColor : handleRandomRbgColor
+          typeOfColor === "hex" ? handleRandomHexColor : handleRandomRbgColor
         }>Random Button</button>
            <div style={{display:"flex", justifyContent: "center", color: "white" ,marginLeft: '10%',fontSize:'30px',marginBottom:"50px"
            }}>
-            <h3>{typeOfColor === "rgb"? "RGB" : "HEX"}</h3>
+            <h3>{typeOfColor === "rgb"? "" : "HEX"}</h3>
             <h3>{color}</h3>
            </div>
 
