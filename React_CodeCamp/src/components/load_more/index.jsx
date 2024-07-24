b3a616b0f8205afc8c84e00dbcc28be5ae01d229
Saurav <sauravgamer441@gmail.com>
2024-07-25 00:32:21 +0545

    successfully loaded images and added styles to product divs

--- React_CodeCamp/src/components/load_more/index.jsx
+++ React_CodeCamp/src/components/load_more/index.jsx
@@ -1,4 +1,5 @@
 import {useState , useEffect} from "react"
+import "./styles.css"
 
 
 export default function LoadMore(){
@@ -9,22 +10,51 @@ export default function LoadMore(){
    
    async function fetchProducts(){
     try{
+        setLoading(true)
        const response = 
        await fetch(`https://dummyjson.com/products?limit=20&skip=${count === 0 ? 0 : count*20}`)
 
        const result = await  response.json() 
+       if(result && result.products && result.products.length){
+        setProducts(result.products)
+        setLoading(false)
+       }
+
+
+
     }catch(e){
         console.log(e)
+        setLoading(false)
     }
    }
    
    
    useEffect(() => {
     fetchProducts()
-   },[count])
+   },[])
 
 
-    return <div className="container">
 
+   if(loading){
+    return <div>Loading data ! ! !</div>
+   }
+
+
+
+
+
+    return <div className="load-more-container">
+     <div className="product-container">
+         {
+            products && products.length ?
+            products.map((item) =>(<div className="product"   key={item.id}> 
+                 <img
+                 src={item.thumbnail} alt = {item.title}/>
+                 <p>{item.title}</p>
+
+            </div>)):null
+         }
+     </div>
+     <div className="button-container"><button>Load More</button></div>
     </div>
 }
