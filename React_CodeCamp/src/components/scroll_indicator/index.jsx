a77e5e3e973f40518079fa4393b5633805540fe7
Saurav <sauravgamer441@gmail.com>
2024-07-26 14:36:30 +0545

    + functional scroll progress bar + stylings

--- React_CodeCamp/src/components/scroll_indicator/index.jsx
+++ React_CodeCamp/src/components/scroll_indicator/index.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import "./styles.css"
 
 
 export  default function  ScrollIndicator({url}){
@@ -7,7 +8,7 @@ export  default function  ScrollIndicator({url}){
     const [loading,setLoading] = useState(false)
     const [errorMessage , setErrorMessage] = useState("")
     const [scrollPercentage, setScrollPercentage] = useState(0)
-
+    
     async function fetchData(getUrl){
         try{
             setLoading(true)
@@ -20,32 +21,48 @@ export  default function  ScrollIndicator({url}){
             }
               
         }catch(e)
-       { console.log(e)}
+       { console.log(e)
+        setErrorMessage(e.message)
+       }
     }
-
-
+    
     useEffect(()=> {
         fetchData(url)
     },[url])
 
     function handleScrollPercentage(){
-     const scrollCounter = document.body.scrollTop || document.documentElement.scrollTop
-
-     const height = document.documentElement.scrollHeight - document.documentElement.clientHeight
-
-     setScrollPercentage((scrollCounter/height)*100)
+        const scrollCounter = document.body.scrollTop || document.documentElement.scrollTop
+        
+        const height = document.documentElement.scrollHeight - document.documentElement.clientHeight
+        
+        setScrollPercentage((scrollCounter/height)*100)
     }
-      console.log(scrollPercentage)
+    
     useEffect(()=>
-   {window.addEventListener('scroll', handleScrollPercentage)
+   {window.addEventListener("scroll", handleScrollPercentage)
     
     return ()=>
-   {window.removeEventListener('scroll',{})}
+   {window.removeEventListener("scroll",() => {})}
     },[])
+
+    if(loading)
+        return <div>Loading !!!</div>
+    if(errorMessage)
+       return <div>{errorMessage}</div>
 
     return (
         <div>
+            <div className="top-container">
+
         <h1>Scroll Indicator</h1>
+        <div className="scroll-progress-tracking-container">
+            <div className="current-progress-bar" style={{width : `${scrollPercentage}%`}}>
+                
+            </div>
+
+            </div>
+
+        </div>
             <div className="data-container">
         {
             data && data.length > 0 ? 
