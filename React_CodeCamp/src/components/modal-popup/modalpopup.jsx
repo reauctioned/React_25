7b1028cbe49fb9c0f6413f6939b2627dcda96fbf
Saurav <sauravgamer441@gmail.com>
2024-07-28 17:35:36 +0545

    + mws pop up implemented

--- React_CodeCamp/src/components/modal-popup/modalpopup.jsx
+++ React_CodeCamp/src/components/modal-popup/modalpopup.jsx
@@ -1,7 +1,22 @@
 
 
-export default function Modal(){
-    return <div id=>
+export default function Modal({id, header , body , footer }){
+    return <div id={ id || 'Modal'} className="modal">
+        <div className="modal-content">
+            <div className="header">
+                    <span className="close-modal-icon">&times;</span>
+                    <h2>{header ? header : 'this is header'}</h2>
+            </div>
+            <div className="body">
+               {
+                body ? body : "body"
+               }
+            </div>
+            <div className="footer">
+                {footer ? footer : "footer"}
+            </div>
+
+        </div>
         
     </div>
 }
