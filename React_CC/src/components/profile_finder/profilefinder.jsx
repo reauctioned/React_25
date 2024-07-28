fb8830a002886823d8a7c491cfdba1b5688c33e3
Saurav <sauravgamer441@gmail.com>
2024-07-28 23:03:06 +0545

    + functional look up + clean up + minor stylings

--- React_CC/src/components/profile_finder/profilefinder.jsx
+++ React_CC/src/components/profile_finder/profilefinder.jsx
@@ -18,6 +18,7 @@ import "./styles.css"
             const res = await fetch(`https://api.github.com/users/${userName}`)
 
             const data = await res.json()
+            console.log(data)
             if(data){
                 setUserData(data)
                 setLoading(false)
