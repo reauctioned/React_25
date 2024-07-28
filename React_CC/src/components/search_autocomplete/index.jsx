abf6569cca546ae270e4383ca6030236271ea235
Saurav <sauravgamer441@gmail.com>
2024-07-28 23:19:53 +0545

    + mapped firstname only

--- React_CC/src/components/search_autocomplete/index.jsx
+++ React_CC/src/components/search_autocomplete/index.jsx
@@ -12,14 +12,21 @@ export default function SearchAutoComplete(){
 
     async function fetchUserList(){
         try{
+            setLoading(true)
             const res = await fetch('https://dummyjson.com/users')
 
             const data = await res.json()
+
+            if(data && data.users && data.users.length){
+                setUsers(data.users.map(()=> userItem.firstName))
+                setLoading(false)
+                setError(null)
+            }
         }catch(error)
         {setError(error)}
     }
 
-
+    
 
      useEffect(()=> {
         fetchUserList()
