422cd246859b5f1451a60a3d345ace9d66c72e5b
Saurav <sauravgamer441@gmail.com>
2024-07-31 23:32:35 +0545

    + fetched data and the required keys

--- React_CC/src/components/feature_flag/context/index.jsx
+++ React_CC/src/components/feature_flag/context/index.jsx
@@ -1,4 +1,5 @@
-import { createContext } from "react"
+import { createContext, useEffect, useState } from "react"
+import featureFlagsDataCall from "../data"
 
 
 
@@ -8,6 +9,25 @@ import { createContext } from "react"
 export const FeatureFlagContext = createContext(null)
 
 export default function FeatureFlagGobalState({children}){
+    const [loading,setLoading]= useState(false)
+    const [enableFlags, setEnableFlags]= useState({})
+
+   async function fetchFeatureFlags(){
+    try{
+      const res = await featureFlagsDataCall()
+      console.log(res);
+    }catch(error){
+        console.log(error);
+        throw new Error(error)
+    }
+   }
+
+
+
+   useEffect(()=>{
+    fetchFeatureFlags()
+   },[])
+
     return (
         <FeatureFlagContext.Provider value={{}}>
             {children}
