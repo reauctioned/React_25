7547c1cbd48448738c071d5abe3e559556ccba25
Saurav <sauravgamer441@gmail.com>
2024-08-03 11:51:20 +0545

    + added ref for the dif to allow close state when clicked outside

--- React_CC/src/components/use_outsideClick/test.jsx
+++ React_CC/src/components/use_outsideClick/test.jsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import useOutsideClick from ".";
 
 export default function UseOnClickOutsideTest() {
   const [showContent, setShowContent] = useState(false);
+  const ref = useRef();
+  useOutsideClick(ref, () => setShowContent(false));
 
   return (
     <div>
       {showContent ? (
-        <div>
+        <div ref={ref}>
           <h1>Random Content</h1>
           <p>Cloes content by clicking outside the div</p>
         </div>
