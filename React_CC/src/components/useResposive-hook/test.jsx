d477147fae2a07560d8f3c7cc179af76dff0e9cb
Saurav <sauravgamer441@gmail.com>
2024-08-05 22:16:31 +0545

    + called windowsResize

---
+++ React_CC/src/components/useResposive-hook/test.jsx
@@ -0,0 +1,19 @@
+import useWindowResize from "."
+
+
+
+export default function UseWindowResizeTest(){
+
+
+    const windowSize = useWindowResize()
+    
+
+
+    return (
+        <div>
+            <h1>Resize Hook</h1>
+            <p>Width is {width}</p>
+            <p>Height is {height}</p>
+        </div>
+    )
+}
