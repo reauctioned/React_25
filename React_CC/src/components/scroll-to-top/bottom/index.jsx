1f142bb011e543b0f163dd9d668838e625f302d8
Saurav <sauravgamer441@gmail.com>
2024-08-05 22:57:42 +0545

    +scroll to top functionality

--- React_CC/src/components/scroll-to-top/bottom/index.jsx
+++ React_CC/src/components/scroll-to-top/bottom/index.jsx
@@ -14,10 +14,26 @@ export default function ScrollToTB() {
         return <h1>Error!</h1>;
     }
 
+    function handleScrollTop(){
+        window.scrollTo({
+            top:0,
+            left: 0,
+            behavior: "instant"
+        })
+    }
+    function handleScrollBottom(){
+        window.scrollTo({
+            bottom:0,
+            left: 0,
+            behavior: "instant"
+        })
+    }
+
+
     return (
         <div>
             <h1>Scroll To Top & Bottom</h1>
-            <button>Bottom</button>
+            <button onClick={handleScrollBottom}>Bottom</button>
             <h3>This is top section</h3>
             <ul style={{listStyle: 'none'}}>
                 {data && data.products && data.products.length > 0 
@@ -26,7 +42,7 @@ export default function ScrollToTB() {
                 }
             </ul>
             <h3>This is Bottom section</h3>
-            <button>Top</button>
+            <button onClick={handleScrollTop}>Top</button>
         </div>
     );
 }
