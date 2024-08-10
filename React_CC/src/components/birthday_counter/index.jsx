738444e91c95ad1e3fe0f1cadbd6f3debbaa4e4d
Saurav <sauravgamer441@gmail.com>
2024-08-10 10:17:49 +0545

    + function to clear birthdays

--- React_CC/src/components/birthday_counter/index.jsx
+++ React_CC/src/components/birthday_counter/index.jsx
@@ -2,13 +2,22 @@
 import { useState } from "react";
 import "./list.css";
 import List from "./list";
+import data from "./data";
 
 export default function BirthdayCounter() {
-  return (<div className="wrapper">
+    
+    const [people,setPeople]= useState(data);
+    
+   function handleClear(){
+        setPeople([])
+    }
+        
+
+  return (<div className="bg-slate-500  rounded-md">
     <div className="container-birthday">
-        <h3>0 Birthdays Today</h3>
-        <List/>
-        <button   className="w-20 h-10 p-0 mx-5 my-2"  onClick={()=> console.log('clicked')}>Clear</button>
+        <h3>{people.length} Birthdays Today</h3>
+        <List people={people}/>
+        <button   className="w-20 h-10 p-0 mx-5 my-2"  onClick={handleClear}>Clear</button>
     </div>
   </div>) 
 }
--- React_CC/src/components/birthday_counter/list.jsx
+++ React_CC/src/components/birthday_counter/list.jsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
 import "./list.css";
 
-export default function List() {
+export default function List({people}) {
   return (<div>
-    <h2>List Component</h2>
+    {people.map((person)=> {
+        const {name,age,image,id} = person
+        return <article key={id} className="flex-1  flex-column">
+                <img src={image} alt={name} className="w-10 h-10 mx-10" />
+                <div>
+                    <h4>{name}</h4>
+                    <p>{age} years</p>
+                </div>
+        </article>
+    })}
   </div>
   ) 
 }
